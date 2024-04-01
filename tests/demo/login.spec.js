c86d1ce8f50a181dbc72b68c3bd400062be23614
Rahul Ranpura <rahilranpura0697@gmail.com>
2024-04-01 22:23:35 +0530

    changes

--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 const { LoginPage  } = require('../../pages/loginPage');
 const config = require('../../environment_config/config.json');
 
-const url = config.url;
+const url = config.env.url;
 const username = config.env.credentials.username;
 const password = config.env.credentials.password;
 
